17f7a5955a562ddd4464328b095674484325db53
lmt1102011 <phamlang10060@gmail.com>
2025-12-10 18:37:34 +0700

    fix ennd

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,12 @@
+// Firebase config for deployed project (sungjintrb)
+// This file is generated from local env (`ggapi.env`) and should be gitignored.
 window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyDAtjOUgMkrZ0FRIaod0QV9b03qZZnlhu0",
-  authDomain: "messend-online.firebaseapp.com",
-  databaseURL: "https://messend-online-default-rtdb.firebaseio.com",
-  projectId: "messend-online",
-  storageBucket: "messend-online.appspot.com",
-  messagingSenderId: "103927627337",
-  appId: "1:103927627337:web:f5347fc34dd8de4ee00998",
-  measurementId: "G-VV5TBKZ43B"
+  apiKey: "AIzaSyAMdKTlUysMHbskT0Asvsrin36ZUyQwhRE",
+  authDomain: "sungjintrb.firebaseapp.com",
+  databaseURL: "https://sungjintrb-default-rtdb.firebaseio.com",
+  projectId: "sungjintrb",
+  storageBucket: "sungjintrb.firebasestorage.app",
+  messagingSenderId: "977752764730",
+  appId: "1:977752764730:web:06458102edc479cd766003",
+  measurementId: "G-BQQCS20HJR"
 };
