cf5367072d2fb4eba6b27dff30231277684121c1
lmt1102011 <phamlang100160@gmail.com>
2025-12-10 13:05:22 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+window.FIREBASE_CONFIG = {
+  "apiKey": "AIzaSyAMdKTlUysMHbskT0Asvsrin36ZUyQwhRE",
+  "authDomain": "sungjintrb.firebaseapp.com",
+  "databaseURL": "https://sungjintrb-default-rtdb.firebaseio.com",
+  "projectId": "sungjintrb",
+  "storageBucket": "sungjintrb.firebasestorage.app",
+  "messagingSenderId": "977752764730",
+  "appId": "1:977752764730:web:06458102edc479cd766003",
+  "measurementId": "G-BQQCS20HJR"
+};
